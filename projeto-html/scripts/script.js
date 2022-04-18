7a4d5aa46174808d673cdb849ea4f941b68ccf7e
BrennoOli <aceitad@gmail.com>
2022-04-18 13:34:09 -0300

    button stuffs updated

--- projeto-html/scripts/script.js
+++ projeto-html/scripts/script.js
@@ -1,14 +1,13 @@
 var imgGatos = document.getElementById('imgGatos');
 var button = document.getElementById('buttonChangeCat');
 
-const getData = async (el) => {
+const getData = async () => {
     let response = await fetch('https://thatcopy.pw/catapi/rest/')
     let dataJson = await response.json()
-
-    el.src = dataJson.url
+    return dataJson.url
 }
-button.addEventListener('click', e => {
-    getData(imgGatos)
+button.addEventListener('click', async e => {
+    imgGatos.src = await getData()
 })
 
 
